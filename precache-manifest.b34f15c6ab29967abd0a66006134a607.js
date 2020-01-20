22b2478bff32563708a125857c08f0eeda425eb3
wilson espina <wilson.espina@gmail.com>
2020-01-20 21:43:57 +0000

    Updates

---
+++ precache-manifest.b34f15c6ab29967abd0a66006134a607.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13de797ae8ef6de240556a4986d5a054",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "59ec558c74a012718500",
+    "url": "/portfolio-site/static/css/main.a1308bd0.chunk.css"
+  },
+  {
+    "revision": "09f53ed66f526ebd50c5",
+    "url": "/portfolio-site/static/js/2.6e5b4a45.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio-site/static/js/2.6e5b4a45.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59ec558c74a012718500",
+    "url": "/portfolio-site/static/js/main.0888232c.chunk.js"
+  },
+  {
+    "revision": "9f14ee726983e5e1c682",
+    "url": "/portfolio-site/static/js/runtime-main.9b26440e.js"
+  },
+  {
+    "revision": "26882d488330ad58e92fc3a2a2fff2a0",
+    "url": "/portfolio-site/static/media/tube_wallpaper.26882d48.jpg"
+  }
+]);
