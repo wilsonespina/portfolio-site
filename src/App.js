052515e8576a9ff3c9d0cc77721f6c6f5c542c31
wilson espina <wilson.espina@gmail.com>
2020-01-20 20:27:18 +0000

    Add placeholder homepage.

--- src/App.js
+++ src/App.js
@@ -8,16 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          WORK IN PROGRESS
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Updated Portfolio Coming Soon...
+        </p>
+
       </header>
     </div>
   );
